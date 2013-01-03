83bf0e190a0de845f20fe7b7784471353f3efcac
unknown <jenskolind@nhs.net>
2013-01-03 17:51:15 +0000

    Added ability to create specific job descriptions. This is a placeholder for further work such as actually displaying them

--- wp-content/themes/royston-scouts/functions.php
+++ wp-content/themes/royston-scouts/functions.php
@@ -20,9 +20,44 @@
 	);
 	add_theme_support( 'custom-header', $args );
 	
+	function create_post_type() {
+		$labels = array(
+			'name' => _x('Volunteer Roles', 'post type general name'),
+			'singular_name' => _x('Voluneteer Role', 'post type singular name'),
+			'add_new' => _x('Add New', 'role'),
+			'add_new_item' => __('Add New Role'),
+			'edit_item' => __('Edit Role'),
+			'new_item' => __('New Role'),
+			'view_item' => __('View Role'),
+			'search_items' => __('Search Roles'),
+			'not_found' =>  __('Nothing found'),
+			'not_found_in_trash' => __('Nothing found in Trash'),
+			'parent_item_colon' => ''
+		);
+	 
+		$args = array(
+			'labels' => $labels,
+			'public' => true,
+			'publicly_queryable' => true,
+			'show_ui' => true,
+			'query_var' => true,
+			'menu_icon' => get_stylesheet_directory_uri() . '/article16.png',
+			'rewrite' => true,
+			'capability_type' => 'post',
+			'hierarchical' => false,
+			'menu_position' => null,
+			'supports' => array('title','editor','thumbnail')
+		  ); 
+ 
+		register_post_type( 'scout_role' , $args );
+	}
+	
 	function royston_scouts_theme_setup() {
+		add_action( 'init', 'create_post_type' );
 		remove_filter( 'wp_page_menu_args', 'responsive_page_menu_args' );
+		add_filter('widget_text', 'do_shortcode');
 	}
 
 	add_action( 'after_setup_theme', 'royston_scouts_theme_setup' );
+	add_action( 'after_switch_theme', 'flush_rewrite_rules' );
 ?>
