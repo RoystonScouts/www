fe4ef79e1a1f2db4839eb184946c89e390fd815f
root <root@ubuntu.(none)>
2013-02-25 17:47:14 +0000

    Theme cleanup

--- wp-content/themes/royston-scouts/functions.php
+++ wp-content/themes/royston-scouts/functions.php
@@ -1,7 +1,7 @@
 <?php
 
-// Exit if accessed directly
-if ( !defined('ABSPATH')) exit;
+	// Exit if accessed directly
+	if ( !defined('ABSPATH')) exit;
 
 	require ( get_stylesheet_directory() . '/includes/image_sizes.php' );
 	require ( get_stylesheet_directory() . '/includes/menus.php' );
@@ -11,7 +11,6 @@ if ( !defined('ABSPATH')) exit;
 	function royston_scouts_theme_setup() {
 		add_theme_support( 'post-thumbnails' );
 		add_filter('widget_text', 'do_shortcode');
-//		remove_filter( 'wp_page_menu_args', 'responsive_page_menu_args' );
 	}
 
 	add_action( 'after_setup_theme', 'royston_scouts_theme_setup' );
